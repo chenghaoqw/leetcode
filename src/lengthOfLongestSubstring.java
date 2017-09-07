70dd72a75e18c8c38ccdd94977a3ba69a1a2f7d8
chenghao05 <chenghao05@baidu.com>
2017-09-07 20:13:56 +0800

    "lengthOfLongestSubstring"

---
+++ src/lengthOfLongestSubstring.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+class lengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        int len=s.length();
+        int flag=0;
+        int result=0;
+        int nowResult=0;
+        for(int i=0;i<len;i++){
+            if(map.get(s.charAt(i))==null){
+                nowResult+=1;
+                map.put(s.charAt(i),i);
+                if(nowResult>result){
+                    result=nowResult;
+                }
+            }else{
+                int j=map.get(s.charAt(i));
+                map.put(s.charAt(i),i);
+                if(j<flag){
+                    j=flag;
+                }else{
+                    flag=j;
+                }
+                nowResult=i-j;
+                if(nowResult>result){
+                    result=nowResult;
+                }
+
+            }
+        }
+        return result;
+    }
+}
