4d7dae3d811d2b02dfd7e4a25ba54c511fa79ec2
chenghaoqw <chenghaplki@163.com>
2017-09-13 20:58:04 +0800

    "longestPaliindrome accepted dp"

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -9,33 +9,32 @@ public class LongestPalindrome {
 
     public static String longestPalindrome1(String s) {
         int l = s.length();
-        String re = new StringBuffer(s).reverse().toString();
-        int[][] result = new int[l + 1][l + 1];
-        result[0][0] = 0;
-        int i, j;
-        int start, end = 0;
+        int start = 0;
+        int end = 0;
         int max = 0;
-        for (i = 1; i <= l; i++) {
-            for (j = 1; j <= l; j++) {
-                if (re.charAt(j - 1) == s.charAt(i - 1)) {
-                    result[i][j] = result[i - 1][j - 1] + 1;
-                    System.out.printf("i:" + i +" "+ "j:" + j + " max:" + max);
-                    System.out.printf("\n");
-                    StringBuffer temp = new StringBuffer(s.substring(l - j, l - j + result[i][j]));
-                    StringBuffer temp1 = new StringBuffer(s.substring(i - result[i][j], i));
-                    System.out.printf(temp.toString() + "  " + temp1.toString() + " " + temp.toString().equals(temp1.toString()));
-                    System.out.printf("\n");
-                    if (result[i][j] > max && temp.toString().equals(temp1.toString())) {
-//                    if (result[i][j] > max) {
-                        start = i;
-                        end = j;
-                        max = result[i][j];
-                    }
-                } else {
-                    result[i][j] = 0;
+        int[][] data = new int[l][l];
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+                if (i >= j) {
+                    data[i][j] = 1;
                 }
             }
         }
-        return re.substring(end - max, end);
+        for (int i = 1; i < l; i++) {
+            for (int j = 0; i + j < l; j++) {
+                if (s.charAt(j) == s.charAt(j + i) && data[j + 1][i + j - 1] == 1) {
+                    data[j][i + j] = 1;
+                    if (i + 1 > max) {
+                        start = j;
+                        end = i + j;
+                        max = i + 1;
+                    }
+                }
+            }
+        }
+        // return start+"";
+
+        return s.substring(start, end + 1);
+
     }
 }
