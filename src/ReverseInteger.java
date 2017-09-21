50572cfccd4956c7c25113e262eebc4d7152f8a7
chenghaoqw <chenghaplki@163.com>
2017-09-21 19:15:41 +0800

    7.Reverse Integer accepted

---
+++ src/ReverseInteger.java
@@ -0,0 +1,22 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        boolean isMinus = false;
+        if (x < 0) {
+            isMinus = true;
+            x = x * -1;
+        }
+        long result = 0;
+        while (x != 0) {
+            result = result * 10;
+            result += x % 10;
+            x = x / 10;
+        }
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        }
+        if (isMinus) {
+            result = result * -1;
+        }
+        return (int) result;
+    }
+}
