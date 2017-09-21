e6299b6055eb51fabe15e46b5f14dd5e23262e3d
chenghaoqw <chenghaplki@163.com>
2017-09-21 19:23:01 +0800

    8.String to Integer Medium accepted notice + -and overflow

---
+++ src/StringtoInteger.java
@@ -0,0 +1,45 @@
+public class StringtoInteger {
+    public int myAtoi(String str) {
+        int l=str.length();
+        if(l==0){
+            return 0;
+        }
+        long result=0;
+        boolean start=false;
+        boolean minus=false;
+        for(int i=0;i<l;i++){
+            char temp=str.charAt(i);
+            if (temp >='0' && temp <='9'){
+                result*=10;
+                result+=temp-'0';
+                start=true;
+                if(minus){
+                    if(result*-1<Integer.MIN_VALUE){
+                        result=Integer.MIN_VALUE;
+                        break;
+                    }
+                }else{
+                    if(result>Integer.MAX_VALUE){
+                        result=Integer.MAX_VALUE;
+                        break;
+                    }
+                }
+            }else {
+                if(temp==' ' && !start){
+                    continue;
+                }else if((temp=='-' || temp=='+') && !start){
+                    if(temp=='-'){
+                        minus=true;
+                    }
+                    start=true;
+                }else{
+                    break;
+                }
+            }
+        }
+        if(minus){
+            result*=-1;
+        }
+        return (int) result;
+    }
+}
