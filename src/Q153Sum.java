9885e0382f3b2ebf0b0c811d35fd78007ff0a435
chenghaoqw <chenghaplki@163.com>
2017-10-18 13:24:51 +0800

    [add accepted]3Sum Medium

---
+++ src/Q153Sum.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q153Sum {
+    private List<List<Integer>> result=new ArrayList<List<Integer>>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(nums==null || nums.length<3){
+            return result;
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            find(nums[i],nums,i+1,nums.length-1);
+        }
+        return result;
+
+    }
+
+    public void find(int target,int[] nums,int begin,int end){
+        for(;end>begin;){
+            int temp=nums[end] + nums[begin];
+            if(temp>-target){
+                end--;
+                while(end >begin && nums[end] ==nums[end+1]){
+                    end--;
+                }
+            }else if(temp<-target){
+                begin++;
+                while(end >begin && nums[begin] ==nums[begin-1]){
+                    begin++;
+                }
+            }else{
+                List<Integer> data=new ArrayList<Integer>();
+                data.add(target);
+                data.add(nums[begin]);
+                data.add(nums[end]);
+                result.add(data);
+                end--;
+                while(end >begin && nums[end] ==nums[end+1]){
+                    end--;
+                }
+                begin++;
+                while(end >begin && nums[begin] ==nums[begin-1]){
+                    begin++;
+                }
+            }
+        }
+    }
+}
