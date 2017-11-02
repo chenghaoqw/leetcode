827c8688a4bfd44c0fad8a792aebdc2f5cafd4eb
chenghaoqw <chenghaplki@163.com>
2017-11-02 23:22:39 +0800

    [add accepted]Array Easy——Majority Element

---
+++ src/Q169MajorityElement.java
@@ -0,0 +1,22 @@
+public class Q169MajorityElement {
+    public int majorityElement(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int temp=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] !=temp){
+                count--;
+                if(count<=0){
+                    temp=nums[i];
+                    count=1;
+                }
+            }else{
+                count++;
+            }
+
+        }
+        return temp;
+    }
+}
