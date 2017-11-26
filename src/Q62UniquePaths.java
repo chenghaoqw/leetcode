05735e2cc1facead25b4e5131b390b1128ff947f
chenghaoqw <chenghaplki@163.com>
2017-11-26 21:49:24 +0800

    [add accepted]Array Medium——Unique Paths

--- src/Q62UniquePaths.java
+++ src/Q62UniquePaths.java
@@ -1,2 +1,15 @@
 public class Q62UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] r=new int[m+1][n+1];
+        r[1][1]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 && j==1){
+                    continue;
+                }
+                r[j][i]=r[j-1][i] + r[j][i-1];
+            }
+        }
+        return r[m][n];
+    }
 }
