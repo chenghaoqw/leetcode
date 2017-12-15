d6b41eab8bb039d08abd3ea044536370893efaa2
chenghaoqw <chenghaplki@163.com>
2017-12-15 13:13:22 +0800

    [add accepted]Array Medium——Minimum Size Subarray Sum

---
+++ src/Q209MinimumSizeSubarraySum.java
@@ -0,0 +1,15 @@
+public class Q209MinimumSizeSubarraySum {
+    public int minSubArrayLen(int s, int[] nums) {
+        int res=Integer.MAX_VALUE;
+        int left=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            while(sum>=s){
+                res=Math.min(res,i-left+1);
+                sum-=nums[left++];
+            }
+        }
+        return res==Integer.MAX_VALUE?0:res;
+    }
+}
