bbd4277f63c9bbfdde188ee8e4fc387ba88f0519
chenghaoqw <chenghaplki@163.com>
2018-01-05 13:53:10 +0800

    [add accepted]Array Medium——Maximum Length of Repeated Subarray

--- src/Q718MaximumLengthofRepeatedSubarray.java
+++ src/Q718MaximumLengthofRepeatedSubarray.java
@@ -1,2 +1,15 @@
 public class Q718MaximumLengthofRepeatedSubarray {
+    public int findLength(int[] A, int[] B) {
+        int ans = 0;
+        int[][] r = new int[A.length + 1][B.length + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    r[i][j] = r[i - 1][j - 1] + 1;
+                    ans = Math.max(r[i][j], ans);
+                } else r[i][j] = 0;
+            }
+        }
+        return ans;
+    }
 }
