ba1d61c3da6d8e43af88e3dc40e24fc5a35d81ec
chenghaoqw <chenghaplki@163.com>
2018-03-14 19:04:30 +0800

    [add accepted]Math Easy——Valid Perfect Square

---
+++ src/Q367ValidPerfectSquare.java
@@ -0,0 +1,10 @@
+public class Q367ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        for(int i=0;i<=num/2;i++){
+            if(i * i==num) return true;
+            else if(i * i>num) return false;
+        }
+        return false;
+    }
+}
