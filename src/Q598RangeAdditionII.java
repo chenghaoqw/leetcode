b4608d1c309d471d16d18ee6ae88c9f611cd3c79
chenghaoqw <chenghaplki@163.com>
2018-03-19 10:59:40 +0800

    [add accepted]Math Easy—Range Addition II

---
+++ src/Q598RangeAdditionII.java
@@ -0,0 +1,11 @@
+public class Q598RangeAdditionII {
+    public int maxCount(int m, int n, int[][] ops) {
+        int r=m;
+        int c=n;
+        for(int i=0;i<ops.length;i++){
+            r=Math.min(r,ops[i][0]);
+            c=Math.min(c,ops[i][1]);
+        }
+        return r*c;
+    }
+}
