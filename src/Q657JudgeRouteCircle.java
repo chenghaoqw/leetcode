d25bb1dffad430cdaf7885be67b5e308a802cf34
chenghaoqw <chenghaplki@163.com>
2018-05-13 19:17:36 +0800

    [add accepted]String Easy—Judge Route Circle

---
+++ src/Q657JudgeRouteCircle.java
@@ -0,0 +1,13 @@
+public class Q657JudgeRouteCircle {
+    public boolean judgeCircle(String moves) {
+        int h=0,v=0;
+        for(int i=0;i<moves.length();i++){
+            char c=moves.charAt(i);
+            if(c=='U') v++;
+            else if(c=='D') v--;
+            else if(c=='L') h--;
+            else if(c=='R') h++;
+        }
+        return v==0 && h==0;
+    }
+}
