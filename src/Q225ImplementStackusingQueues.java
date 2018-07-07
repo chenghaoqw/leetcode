20eb9c4ca7b03689ef017de1aa5215b455a9da82
VHungry <chenghaplki@163.com>
2018-07-07 14:13:13 +0800

    [add accepted]Easy——Implement Stack using Queues

---
+++ src/Q225ImplementStackusingQueues.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+public class Q225ImplementStackusingQueues {
+    LinkedList<Integer> queue;
+    /** Initialize your data structure here. */
+    public Q225ImplementStackusingQueues() {
+        queue = new LinkedList<Integer>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.offer(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.removeLast();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.getLast();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
