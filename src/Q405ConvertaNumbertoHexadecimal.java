7722bb6157d0d2638a29d99eaaf07ee8c7affa9a
VHungry <chenghaplki@163.com>
2018-07-15 14:21:01 +0800

    [add accepted]Easy——Convert a Number to Hexadecimal

---
+++ src/Q405ConvertaNumbertoHexadecimal.java
@@ -0,0 +1,12 @@
+public class Q405ConvertaNumbertoHexadecimal {
+    public String toHex(int num) {
+        if(num==0) return "0";
+        char[] map = new char[]{'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        String result="";
+        while(num !=0){
+            result=map[num & 0XF]+result;
+            num=num >>> 4;
+        }
+        return result;
+    }
+}
