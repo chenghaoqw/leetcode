639d92a7f695a5832dba3b000f89967f7cec6c4b
VHungry <chenghaplki@163.com>
2018-09-23 10:25:33 +0800

    [add accepted]Easy——Jewels and Stones

---
+++ src/Q771JewelsandStones.java
@@ -0,0 +1,9 @@
+public class Q771JewelsandStones {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (J.contains(S.substring(i, i + 1))) count++;
+        }
+        return count;
+    }
+}
