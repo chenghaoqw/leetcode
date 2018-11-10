5a2cacf5d9830ec7d2de93408011c41aef0a8ed9
VHungry <chenghaplki@163.com>
2018-11-10 20:44:09 +0800

    [add accepted]Medium——Range Sum Query 2D - Immutable

---
+++ src/Q304RangeSumQuery2DImmutable.java
@@ -0,0 +1,17 @@
+public class Q304RangeSumQuery2DImmutable {
+    int[][] dp;
+
+    public Q304RangeSumQuery2DImmutable(int[][] matrix) {
+        if (matrix.length <= 0) return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + matrix[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
