751daf741a852c3cacffd262caf2a650d9a3bbc8
VHungry <chenghaplki@163.com>
2018-11-28 11:31:20 +0800

    [add accepted]Medium——Combination Sum IV

---
+++ src/Q377CombinationSumIV.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Q377CombinationSumIV {
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j : nums) {
+                if (i - j >= 0) dp[i] += dp[i - j];
+            }
+        }
+        return dp[target];
+    }
+}
