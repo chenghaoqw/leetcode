74074e6fbad2afae422d0f71c687c68dc3535586
chenghaoqw <chenghaplki@163.com>
2018-12-10 23:09:48 +0800

    [add accepted]Medium—Partition Equal Subset Sum

---
+++ src/Q416PartitionEqualSubsetSum.java
@@ -0,0 +1,15 @@
+public class Q416PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = target; i >= num; i--)
+                dp[i] = dp[i] || dp[i - num];
+        }
+        return dp[target];
+    }
+}
