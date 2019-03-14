fc0aa5a2675c4b411724287771cf8cafca0bda6d
VHungry <chenghaplki@163.com>
2019-03-14 15:58:30 +0800

    [add accepted]Medium——Binary Tree Pruning

---
+++ src/Q814BinaryTreePruning.java
@@ -0,0 +1,11 @@
+public class Q814BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = pruneTree(root.left);
+        TreeNode right = pruneTree(root.right);
+        if (left == null) root.left = null;
+        if (right == null) root.right = null;
+        if (left == null && right == null && root.val == 0) return null;
+        return root;
+    }
+}
